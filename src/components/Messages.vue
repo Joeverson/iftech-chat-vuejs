fd022797288cb6bbb6c666c3b1087453bb25cd3a
joerverson.santos <joerverson.santos@gmail.com>
2018-11-14 16:57:43 -0300

    init2

---
+++ src/components/Messages.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class='container' id="screen">
+    <div class="screen">
+      <ul>
+        <li v-for="(message, i) in data" v-bind:key="i" :class="message.side">
+          <small class='my-name is-outlined'>{{ message.author }}</small> 
+          <br>
+          <span>{{ message.text }}</span>
+        </li>      
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'messages',
+    props: {
+      data: Array
+    },
+    updated() {      
+      this.$el.scrollTop = this.$el.scrollHeight;
+    }
+  }
+</script>
+
+<style>
+  .screen {
+    max-height: 500px;
+    overflow-y: scroll;
+  }
+  .my-name {
+    color: #808080
+  }
+  .others{
+    text-align: right
+  }
+  .me{
+    text-align: left
+  }
+</style>
---
+++ src/components/SendMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class='container'>
+    <div class="columns">
+      <div class='column'>
+        <input class="input is-large" v-model="message.text" type="text" name="" id="send-message"/>
+      </div>
+      <div class='column is-one-quarter'>
+        <button class='button is-success is-rounded is-large is-fullwidth' @click='$emit("send-message", Object.assign({}, message))' >Enviar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>  
+  export default {
+    name: 'send-message',
+    data() {
+      return {
+        message: {
+          author: 'joer',
+          who: 'me',
+          text: ''
+        }
+      };
+    }    
+  }
+</script>
